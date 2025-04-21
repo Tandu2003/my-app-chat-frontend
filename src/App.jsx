ccd82beebbbb8825ef8bee46d78b69d3fd1af351
Tấn Dự <tandu542003@gmail.com>
2025-04-21 09:11:30 +0700

    feat: update Ant Design icons to version 6.0.0 and integrate SmileOutlined in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import { Button } from 'antd'
+import { SmileOutlined } from '@ant-design/icons'
 
 const App = () => {
   return (
     <div className="flex h-screen w-screen flex-col items-center justify-center">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="text-3xl font-bold underline">
+        Hello world! <SmileOutlined />
+      </h1>
       <Button type="primary">Ant Design Button</Button>
     </div>
   )

272546722643101286a59732f5962db1bf2a2543
Tấn Dự <tandu542003@gmail.com>
2025-04-21 09:10:07 +0700

    feat: add Ant Design library and integrate Button component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
+import { Button } from 'antd'
+
 const App = () => {
-  return <h1 class="text-3xl font-bold underline">Hello world!</h1>
+  return (
+    <div className="flex h-screen w-screen flex-col items-center justify-center">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Button type="primary">Ant Design Button</Button>
+    </div>
+  )
 }
 
 export default App
